b51a7dbb2afcfea3a384b053374c1ab2e3b80505
Varad Gondepatil <67214045+vgondepatil@users.noreply.github.com>
2024-01-17 21:11:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+let activeTitle = 'You have 6 items';
+let inactiveTitle = 'Please come back';
+document.title = activeTitle;
+window.addEventListener('blur', e => {
+    document.title = inactiveTitle;
+});
+window.addEventListener('focus', e => {
+    document.title = activeTitle;
+});
